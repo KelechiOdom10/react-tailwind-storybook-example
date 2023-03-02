484e5f7adabe8b7cbac39b098c90cec5849733fe
Kelechi Odom <kelechiodom10@gmail.com>
2023-03-02 15:19:14 +0000

    delete unused files and update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Button } from "./components/Button/Button";
+import { Button } from "./components/button/button";
 
 function App() {
   return (
-    <div className={"h-screen w-full container"}>
+    <div className="h-screen w-full p-8 container">
       <Button>mad</Button>
     </div>
   );
